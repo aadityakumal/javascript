ae58bdafa6ec20615b3f2b3dc674ce92831a4daa
aadityakumal <adityakumal626@gmail.com>
2024-12-07 11:40:41 +0545

    first commit

---
+++ html.js
@@ -0,0 +1,17 @@
+// 
+
+
+var id ="473"
+ let user_name="aaditya"
+let class_name="BCSIT"
+ 
+
+ document.getElementById("id").innerHTML=id
+ document.getElementsByName("user_name").innerHTML=user_name
+document.getElementsByClassName("class_name").innerHTML=class_name
+ document.getElementsByTagName("").innerHTML=tagname
+ document.querySelector("").innerHTMl=querySelector
+ Document.querySelectorAll("").innerHTML=selecterAll
+ console.log(id)
+ console.log(user_name)
+ console.log(class_name)
